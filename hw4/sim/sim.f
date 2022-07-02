01371a8d517fba5cbeb265fc721e60d55250f5ef
Li-HAO CHEN <98183102+VincentChen1017@users.noreply.github.com>
2022-07-02 15:59:53 +0800

    Add files via upload

---
+++ hw4/sim/sim.f
@@ -0,0 +1,13 @@
+test_qr_decode.v
+
+./sram_model/sram_4096x1b.v
+
+../hdl/qr_decode.v
+//put your other RTL below
+
+
++access+r
+//+define+PAT_START_NO=0 +define+PAT_END_NO=39 +define+RANK_A
+//+define+PAT_START_NO=0 +define+PAT_END_NO=39 +define+RANK_B1
+//+define+PAT_START_NO=0 +define+PAT_END_NO=9 +define+RANK_B
++define+PAT_START_NO=0 +define+PAT_END_NO=9 +define+RANK_C
